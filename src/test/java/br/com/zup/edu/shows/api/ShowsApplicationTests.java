1a49a883de58ab7ae6a5e65736c2e7ca4ed07e55
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-04-28 09:48:15 -0300

    Commit inicial

---
+++ src/test/java/br/com/zup/edu/shows/api/ShowsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.shows.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
